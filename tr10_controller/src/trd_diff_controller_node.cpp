dde1a9455ca3d0ac02434deac44fe1edd611e870
吴雪铭 <37106995+Xueming10wu@users.noreply.github.com>
2019-04-15 00:35:40 +0800

    Delete trd_diff_controller_node.cpp

--- tr10_controller/src/trd_diff_controller_node.cpp
+++
@@ -1,8 +0,0 @@
-#include "trd_diff_controller/trd_diff_controller.h"
-
-int main(int argc, char* argv[]){
-    ros::init(argc, argv, "trd_diff_controller_node");
-    TRDDiffController trd_diff_controller;
-    return 0;
-}
-
